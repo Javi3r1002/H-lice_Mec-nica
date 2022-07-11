e886f7bca4f22d54d1ccabf05678764a79e2b777
Javi3r1002 <65726905+Javi3r1002@users.noreply.github.com>
2022-07-11 10:08:24 -0600

    Helice
    Sencillo viejo

--- Helice.py
+++ Helice.py
@@ -4,5 +4,23 @@ import math
 from mpl_toolkits.mplot3d import Axes3D
 from math import pi
 
+fig = plt.figure()
+ax1 = fig.add_subplot(111, projection='3d')
+
 R = 1
-Ang = np.lnspace(0,2*pi, .01)
+a = .5
+Ang = np.arange(0,2*pi, .01)
+
+
+x = []
+y = []
+z = []
+
+for i in Ang:
+	x.append(R*math.cos(i))
+	y.append(R*math.sin(i))
+	z.append(((a*R)/(np.sqrt(1-a**2)))*i)
+
+ax1.scatter(x,y,z)
+plt.show()
+
