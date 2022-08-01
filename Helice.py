c77f710a85b3e8bb685042805e01f5e51ad17861
Javi3r1002 <65726905+Javi3r1002@users.noreply.github.com>
2022-07-31 23:00:40 -0600

    SSS

--- Helice.py
+++ Helice.py
@@ -5,11 +5,12 @@ from mpl_toolkits.mplot3d import Axes3D
 from math import pi
 
 fig = plt.figure()
-ax1 = fig.add_subplot(111, projection='3d')
+#ax1 = fig.add_subplot(111, projection='3d')
+ax1 = fig.add_subplot(111)
 
 R = 1
 a = .5
-Ang = np.arange(0,2*pi, .01)
+Ang = np.arange(0,10*pi, .001)
 
 
 x = []
@@ -21,6 +22,10 @@ for i in Ang:
 	y.append(R*math.sin(i))
 	z.append(((a*R)/(np.sqrt(1-a**2)))*i)
 
-ax1.scatter(x,y,z)
+#ax1.scatter(x,y,z)
+ax1.set_xlabel('Ángulo', fontsize = 15)
+ax1.set_ylabel('Áltura', fontsize = 15)
+#ax1.set_zlabel('z', fontsize = 15)
+ax1.scatter(Ang,z)
 plt.show()
 
